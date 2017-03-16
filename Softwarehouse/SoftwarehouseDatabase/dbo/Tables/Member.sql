601b85ca31b2b8fdb66633e8c664f7715f0f2076
WillianHsiao <lucien5335@hotmail.com>
2017-03-16 22:37:18 +0800

    匯入資料庫檔案

---
+++ Softwarehouse/SoftwarehouseDatabase/dbo/Tables/Member.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Member] (
+    [Id]       INT           IDENTITY (1, 1) NOT NULL,
+    [Account]  NVARCHAR (20) NOT NULL,
+    [Password] NVARCHAR (20) NOT NULL,
+    [Name]     NCHAR (10)    NOT NULL,
+    CONSTRAINT [PK_Member] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
