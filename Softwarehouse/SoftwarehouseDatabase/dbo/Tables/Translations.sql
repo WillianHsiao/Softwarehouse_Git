4b99fc03bccf8eb1bc8524193d0bd314a8b05e77
chris.hung <chris.hung@afusion.com>
2017-03-16 21:43:01 +0800

    add sql database project

---
+++ Softwarehouse/SoftwarehouseDatabase/dbo/Tables/Translations.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Translations] (
+    [Id]           UNIQUEIDENTIFIER DEFAULT (newsequentialid()) NOT NULL,
+    [VocabularyId] UNIQUEIDENTIFIER NOT NULL,
+    CONSTRAINT [PK_dbo.Translations] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_dbo.Translations_dbo.Vocabularies_VocabularyId] FOREIGN KEY ([VocabularyId]) REFERENCES [dbo].[Vocabularies] ([Id]) ON DELETE CASCADE
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_VocabularyId]
+    ON [dbo].[Translations]([VocabularyId] ASC);
+
---
+++ Softwarehouse/SoftwarehouseDatabase/dbo/Tables/Vocabularies.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Vocabularies] (
+    [Id]           UNIQUEIDENTIFIER DEFAULT (newsequentialid()) NOT NULL,
+    [SaveTime]     DATETIME         NOT NULL,
+    [IsRemembered] BIT              NOT NULL,
+    CONSTRAINT [PK_dbo.Vocabularies] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
