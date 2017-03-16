a5175b7463c36e9d1cca4fa5d1b032b9027314d1
chris.hung <chris.hung@afusion.com>
2017-03-16 21:54:19 +0800

    clean up

--- Softwarehouse/SoftwarehouseDatabase/dbo/Tables/Vocabularies.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[Vocabularies] (
-    [Id]           UNIQUEIDENTIFIER DEFAULT (newsequentialid()) NOT NULL,
-    [SaveTime]     DATETIME         NOT NULL,
-    [IsRemembered] BIT              NOT NULL,
-    CONSTRAINT [PK_dbo.Vocabularies] PRIMARY KEY CLUSTERED ([Id] ASC)
-);
-
