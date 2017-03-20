da51cc1cf4e0a80a5f88bf9293a90103110c9171
WillianHsiao <lucien5335@hotmail.com>
2017-03-20 23:36:05 +0800

    Member(買方)前台加入DI、註冊會員新增Controller、View、ViewModel

---
+++ Softwarehouse/Softwarehouse/Controllers/RegisterController.cs
@@ -0,0 +1,27 @@
+﻿using Business.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Softwarehouse.Controllers
+{
+    /// <summary>
+    /// 註冊會員Controller
+    /// </summary>
+    public class RegisterController : Controller
+    {
+        private MembersRepository _repository;
+
+        public RegisterController(MembersRepository repository)
+        {
+            _repository = repository;
+        }
+        // GET: Register
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Softwarehouse/Softwarehouse/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Softwarehouse.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Display(Name = "帳號"), Required]
+        public string Account { get; set; }
+        [Display(Name = "密碼"), Required]
+        public string Password { get; set; }
+        [Compare(nameof(Password), ErrorMessage = "與輸入的密碼不符合")]
+        public string ConfirmPassword { get; set; }
+        [DataType(DataType.EmailAddress), Required]
+        public string Email { get; set; }
+    }
+}
