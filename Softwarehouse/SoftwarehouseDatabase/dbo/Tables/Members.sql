5d1cd4a15c77bc3d5e4548d613a176da78ebd9da
chris.hung <chris.hung@afusion.com>
2017-03-21 21:55:43 +0800

    added primary key

--- Softwarehouse/SoftwarehouseDatabase/dbo/Tables/Members.sql
+++ Softwarehouse/SoftwarehouseDatabase/dbo/Tables/Members.sql
@@ -2,6 +2,7 @@
     [Id]       INT           IDENTITY (1, 1) NOT NULL,
     [Account]     NVARCHAR(25)    NOT NULL,
     [Password] NVARCHAR(MAX) NOT NULL, 
-    [Email] NVARCHAR(50) NOT NULL
+    [Email] NVARCHAR(50) NOT NULL, 
+    CONSTRAINT [PK_Members] PRIMARY KEY ([Id])
 );
 
--- Softwarehouse/SoftwarehouseDatabase/dbo/Tables/VipUsers.sql
+++ Softwarehouse/SoftwarehouseDatabase/dbo/Tables/VipUsers.sql
@@ -3,5 +3,6 @@
 	[Id] INT           IDENTITY (1, 1) NOT NULL, 
     [Account] NVARCHAR(25) NOT NULL, 
     [Password] NVARCHAR(50) NOT NULL, 
-    [Email] NVARCHAR(50) NOT NULL
+    [Email] NVARCHAR(50) NOT NULL, 
+    CONSTRAINT [PK_VipUsers] PRIMARY KEY ([Id])
 )
